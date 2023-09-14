59213b3d0c3e6ea654c7ca504447223c0eeed003
Your Name <https://github.com/elgiz17/git_lesson1.git>
2023-09-14 10:01:08 +0600

    comm

--- home_work_1/bin/home4_w.dart
+++ home_work_1/bin/home4_w.dart
@@ -6,5 +6,5 @@ void main() {
   var a = double.parse(stdin.readLineSync() ?? '0');
     
       double b = 32;
-print('Градус Цельсия в Градус Фаренгейта  = ${(a * 9/5) + b}');
+print('Градус Цельсия в Градус Фаренгейта  = ${(a * 9/5) + 32}');
 }
