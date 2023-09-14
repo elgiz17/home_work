d70d52a9dcd80ebb15055629aeb5ad00c5d32cff
Your Name <https://github.com/elgiz17/git_lesson1.git>
2023-09-14 09:45:56 +0600

    commit

--- home_work_1/bin/home_work_1.dart
+++ home_work_1/bin/home_work_1.dart
@@ -1,5 +1,25 @@
-import 'package:home_work_1/home_work_1.dart' as home_work_1;
+// import 'package:home_work_1/home_work_1.dart' as home_work_1;
 
-void main(List<String> arguments) {
-  print('tema');
+// void main(List<String> arguments) {
+//   print('tema');
+// }
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  print('введите число:');
+  
+  var a = int.parse(stdin.readLineSync() ?? '0');
+
+  print("введите число b:");
+
+  var b = int.parse(stdin.readLineSync() ?? '0');
+   print('Результат: $a + $b= ${a+b}');
+
+    print('Результат: $a - $b= ${a-b}');
+
+     print('Результат: $a * $b= ${a*b}');
+
+      print('Результат: $a / $b= ${a/b}');
+  
 }
