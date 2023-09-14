34cc438db552057c65cbb7d64f906d12d9ae544e
Your Name <https://github.com/elgiz17/git_lesson1.git>
2023-09-14 10:07:16 +0600

    commit

---
+++ home_work_1/bin/home5_w.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+
+print(' 1. USD (покупка: 87.0, продажа: 87.25)');
+print(' 2. EUR (покупка: 94.50, продажа: 95.40)');
+print(' 3. RUB (покупка: 0.9000, продажа: 0.9150)');
+
+
+   print('введите сумму: (СОМ = USA)');
+  var a = int.parse(stdin.readLineSync() ?? '0');
+  double c = 87.0;
+ 
+print('$a * 87.0 = ${a*c}');
+
+ double d = 94.50;
+
+
+print('введите сумму: (СОМ = EUR)');
+  var b = int.parse(stdin.readLineSync() ?? '0');
+ print(' $b * 94.50 = ${b*d}');
+
+  double e = 0.9000;
+  
+print('введите сумму: (СОМ = RUB)');
+  var f = int.parse(stdin.readLineSync() ?? '0');
+ print(' $f * 0.9000 = ${f*e}');
+} 
