851f0d5f22164de44ac5ebf5a56050483ffedc8c
Your Name <https://github.com/elgiz17/git_lesson1.git>
2023-09-18 15:01:35 +0600

    commit

---
+++ dart if elde/lesson15_1/bin/lesson15_1.dart
@@ -0,0 +1,16 @@
+import 'package:lesson15_1/lesson15_1.dart' as lesson15_1;
+
+void main(){
+  double a = 12;
+  int b = 12;
+
+  // if  (a<b){
+  // print('a bolwe chem b');
+  // } else if (a>=b) {
+  //   print('b menwe chem a');
+  // }  else if  (a > b) {
+  //   print('chisla ravny');
+  // }
+
+
+}
